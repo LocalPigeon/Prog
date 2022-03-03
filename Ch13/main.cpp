6cee45425295c8278553070a727beef0e2dcaf41
LocalPigeon <90721816+LocalPigeon@users.noreply.github.com>
2022-03-03 09:50:55 +0100

    Added compiler line

--- Ch13/main.cpp
+++ Ch13/main.cpp
@@ -1,6 +1,6 @@
 /*
 g++ -w -Wall -std=c++11 ../GUI/Graph.cpp ../GUI/Window.cpp ../GUI/GUI.cpp ../GUI/Simple_window.cpp main.cpp `fltk-config --ldflags --use-images`
-*/
+*/ 
 
 #include "../GUI/Simple_window.h"
 #include "../GUI/Graph.h"
